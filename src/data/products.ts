47cf24c945e923579cbeba108f2fecf396ca86a2
codeawy <codeawy@gmail.com>
2025-08-11 17:11:52 +0300

    Integrate SavedProductsContext for managing saved products; update ProductCard to toggle saved state and refactor product data structure for consistency.

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,13 +1,52 @@
-import { faker } from "@faker-js/faker";
 import { Product } from "@/interfaces/product";
 
-export const products: Product[] = Array.from({ length: 10 }, () => ({
-  id: faker.string.uuid(),
-  name: faker.commerce.productName(),
-  price: Number(faker.commerce.price()),
-  imageURL: faker.helpers.arrayElement([
-    require("../../assets/images/clothes/product-01.png"),
-    require("../../assets/images/clothes/product-02.png"),
-    require("../../assets/images/clothes/product-03.png"),
-  ]),
-}));
+export const products: Product[] = [
+  {
+    id: "Product 1",
+    name: "Product 1",
+    price: 100,
+    imageURL: require("../../assets/images/clothes/product-01.png"),
+  },
+  {
+    id: "Product 2",
+    name: "Product 2",
+    price: 200,
+    imageURL: require("../../assets/images/clothes/product-02.png"),
+  },
+  {
+    id: "Product 3",
+    name: "Product 3",
+    price: 300,
+    imageURL: require("../../assets/images/clothes/product-03.png"),
+  },
+  {
+    id: "Product 4",
+    name: "Product 4",
+    price: 400,
+    imageURL: require("../../assets/images/clothes/product-01.png"),
+  },
+  {
+    id: "Product 5",
+    name: "Product 5",
+    price: 500,
+    imageURL: require("../../assets/images/clothes/product-01.png"),
+  },
+  {
+    id: "Product 6",
+    name: "Product 6",
+    price: 600,
+    imageURL: require("../../assets/images/clothes/product-02.png"),
+  },
+  {
+    id: "Product 7",
+    name: "Product 7",
+    price: 700,
+    imageURL: require("../../assets/images/clothes/product-03.png"),
+  },
+  {
+    id: "Product 8",
+    name: "Product 8",
+    price: 800,
+    imageURL: require("../../assets/images/clothes/product-01.png"),
+  },
+];
